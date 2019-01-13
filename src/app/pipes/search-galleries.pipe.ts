3e92c5d3a0b4a786b2a09dff226dd8ed90232259
Toom <contact@tombaut.com>
2019-01-13 13:56:05 +0100

    123

--- src/app/pipes/search-galleries.pipe.ts
+++ src/app/pipes/search-galleries.pipe.ts
@@ -15,11 +15,13 @@ export class SearchGalleriesPipe implements PipeTransform {
 
     this.galleries = value;
 
+
     if (args) {
       this.galleries = this.galleries.filter(item =>
         (item.title.indexOf(args) !== -1 || item.description.indexOf(args) !== -1 || item.dateCreated.indexOf(args) !== -1));
     } else {
       this.galleries = value;
+
     }
 
     return this.galleries;
