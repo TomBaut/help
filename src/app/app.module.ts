900ce4b6c1186303119669aa7d01c10b2b45e97b
Toom <contact@tombaut.com>
2019-01-19 20:53:58 +0100

    ćwicz2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PolishDatePipe } from './pipes/polish-date.pipe';
-import { SearchGalleriesPipe } from './pipes/search-galleries.pipe';
 import { NavComponent } from './components/nav/nav.component';
-import { GalleriesComponent } from './components/galleries/galleries/galleries.component';
-import { GalleryItemComponent } from './components/galleries/gallery-item/gallery-item.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PolishDatePipe,
-    SearchGalleriesPipe,
-    NavComponent,
-    GalleriesComponent,
-    GalleryItemComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- src/app/interfaces/IGallery.ts
+++
@@ -1,9 +0,0 @@
-export class IGallery {
-  galleryId: string;
-  title: string;
-  dateCreated: string;
-  thumbUrl: string;
-  description: string;
-  tags: any;
-  photos: any;
-}
